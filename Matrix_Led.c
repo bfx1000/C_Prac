dfd218d10760a12040e587e22eb5911c18277660
bfx1000 <1203896210@qq.com>
2023-02-28 13:06:28 +0800

    8*8 led矩阵，循环显示电子

--- Matrix_Led.c
+++ Matrix_Led.c
@@ -1,19 +1,16 @@
-/**
- * 8*8 led矩阵，循环显示“电子”
- */
 #include <REGX52.H>
 #include <intrins.h>
-sbit DIO = P3 ^ 4;//一位的串行数据，满足非0即1
-sbit SR_CLK = P3 ^ 5;//串行下移
-sbit R_CLK = P3 ^ 6;//输出
+
+sbit DIO = P3 ^ 4;   //一位的串行数据输入端
+sbit SR_CLK = P3 ^ 5;//串行输入开关
+sbit R_CLK = P3 ^ 6; //并行输出开关
 
 
 unsigned char ch[][8] = {
         //电
-        0xF7,0xC1,0xD5,0xC1,0xD5,0xC1,0xB7,0x87,
-
+        0xF7, 0xC1, 0xD5, 0xC1, 0xD5, 0xC1, 0xB7, 0x87,
         //子
-        0x81,0xDF,0xEF,0x00,0xEF,0xDF,0xDB,0xE7
+        0x81, 0xDF, 0xEF, 0x00, 0xEF, 0xDF, 0xDB, 0xE7
 };
 
 void writeByte(unsigned char c) {
@@ -21,9 +18,8 @@ void writeByte(unsigned char c) {
     SR_CLK = 0;
     R_CLK = 0;
     for (i = 0; i < 8; i++) {
-        //DIO 接收先是一列八位数，再是一行八位数，这样就能保持正确
-        DIO = 0x01 & (c >> i);//对c从低到高位依次取值，并将结果串行输入到DIO，取得的结果只能是0或1，所以对DIO直接赋值
-        SR_CLK = 1;        //串行输入
+        DIO = 0x01 & (c >> i);//二进制数单个位结果就是零或一，直接赋值给DIO即可
+        SR_CLK = 1;//串行输入
         SR_CLK = 0;
     }
     //八位输出
@@ -31,19 +27,19 @@ void writeByte(unsigned char c) {
 //    R_CLK = 0;
 }
 
-
 void main() {
-    unsigned char k,row = 0x80;
-    unsigned int j,z=300;
-    while(1){
-        for(k=0;k<2;k++){
-            while(z--){
-                for (j = 0; j < 8; j++) {
-                    writeByte(ch[k][j]);
-                    writeByte(row);//因为共阳极
-                    row=_cror_(row,1);//因为k有2次循环，不能用>>
+    unsigned char k, row;
+    unsigned int j, z;
+    while (1) {
+        for (k = 0; k < 2; k++) {//两个字体，用二维数组表示
+            for (z = 300; z > 0; z--) {//每个字体刷新300次
+                row = 0x80;//300次"电"循环，每次循环重新赋值，避免出现问题
+                for (j = 0; j < 8; j++) {//一个屏幕八列，循环8次
+                    writeByte(ch[k][j]);//控制该列中哪个灯亮，就给低电平
+                    writeByte(row);//哪列灯亮给高电平
                     R_CLK = 1;//并行输出
                     R_CLK = 0;
+                    row = _cror_(row, 1);//让下一列灯亮
                 }
             }
         }
